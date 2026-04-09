0318b159bf2f3456cde4b6f6ae2d88a75d4acd65
devisinghlodhi <devisinghlodhi1999@gmail.com>
2026-04-09 18:41:00 +0530

    fix : move api URL to .env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // REST_API_URL: "http://localhost:5000/api",
-  REST_API_URL : "https://apionlinecode.tk/api",
+  REST_API_URL: process.env.NEXT_PUBLIC_API_URL,
   
 }
 
